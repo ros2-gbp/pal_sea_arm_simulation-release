97bc33606e70638fe2d0036a4a4ac0b8553c8322
Noel Jimenez <noel.jimenez@pal-robotics.com>
2026-06-25 09:55:54 +0000

    Rebase from 'release/humble/pal_sea_arm_gazebo'

--- launch/robot_spawn.launch.py
+++ launch/robot_spawn.launch.py
@@ -12,18 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
+from ament_index_python import get_package_share_directory
+
 from launch import LaunchDescription
-from launch.actions import SetLaunchConfiguration
+from launch.actions import SetLaunchConfiguration, DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from launch.conditions import LaunchConfigurationEquals
 
 from launch_ros.actions import Node
 from dataclasses import dataclass
 from launch_pal.arg_utils import LaunchArgumentsBase
+from launch_pal.robot_arguments import CommonArgs
 
 
 @dataclass(frozen=True)
 class LaunchArguments(LaunchArgumentsBase):
-    pass
+    gazebo_version: DeclareLaunchArgument = CommonArgs.gazebo_version
 
 
 def generate_launch_description():
@@ -49,7 +55,38 @@ def declare_actions(launch_description: LaunchDescription, launch_args: LaunchAr
                                    '-entity', LaunchConfiguration('robot_name'),
                                    "-x", "0.0", "-y", "0.0", "-z", "0.08",
                                    ],
-                        output='screen')
+                        output='screen',
+                                condition=LaunchConfigurationEquals('gazebo_version', 'classic')
+                        )
     launch_description.add_action(robot_entity)
+
+    gazebo_spawn_robot = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-model",
+            LaunchConfiguration("robot_name"),
+            "-topic",
+            "robot_description",
+        ],
+        condition=LaunchConfigurationEquals('gazebo_version', 'gazebo'),
+    )
+    launch_description.add_action(gazebo_spawn_robot)
+
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        name='bridge_ros_gz',
+        parameters=[{
+            'config_file': os.path.join(
+                get_package_share_directory('pal_sea_arm_gazebo'),
+                'config', 'pal_sea_arm_gz_bridge.yaml'),
+            'use_sim_time': True,
+        }],
+        output='screen',
+        condition=LaunchConfigurationEquals('gazebo_version', 'gazebo'),
+    )
+    launch_description.add_action(bridge)
 
     return
